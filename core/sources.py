cd5bc09a6f11ef783a785e462c1da3236d029466
Haozhe Li <hao-zhe.li@outlook.com>
2025-06-25 22:37:03 +0800

    bug fixed

--- core/sources.py
+++ core/sources.py
@@ -6,10 +6,7 @@ class SourcesStore:
     def set_sources(self, sources: list[str]) -> None:
         """Store the sources."""
         # if self.sources is not None:
-        if self.sources:
-            self.sources.extend(sources)
-        else:
-            self.sources = sources
+        self.sources = sources
 
     def get_sources(self) -> list[str]:
         """Retrieve the stored sources."""
